dcc0cbf8ca5beb54a9b175183ac3c1db123dfda1
Edward Deam <edwardcrdeam@gmail.com>
2021-03-25 11:49:07 +1100

    Array Solution

--- ruby/arrays/array.rb
+++ ruby/arrays/array.rb
@@ -17,25 +17,49 @@ INVENTORS = [
 ].freeze
 
 def first_and_last_name(inventors)
+  inventors.map do |inventor|
+    "#{inventor.first} #{inventor.last}"
+  end
 	# return a new array that contains the inventors first and last names as a string
 end
+first_and_last_name(INVENTORS)
 
 def born_before_1800(inventors)
+  inventors.select do |inventor|
+    inventor.born < 1800
+  end
 	# return an array of Inventors who were born before 1800
 end
-
+born_before_1800(INVENTORS)
 def total_age(inventors)
+  inventors.reduce(0) do |total_age, inventor| 
+    puts "Total: #{total_age}"
+    total_age += inventor.passed - inventor.born
+    total_age
+  end
 	# return the total age of all the inventors
 end
-
+total_age(INVENTORS)
 def long_names(inventors)
-	# return an array of Inventors who have a first name of 5 or more letters
+	# return an array of Inventors who have a first name of 6 or more letters
+  inventors.select do |inventor|
+    inventor.first.length > 5 
+  end
 end
-
+long_names(INVENTORS)
 def last_name_sort(inventors)
+  inventors.sort do |inventor_a, inventor_b|
+    inventor_a.last <=> inventor_b.last
+  end
 	# return a new array sorted by the Inventors last name
 end
 
+last_name_sort(INVENTORS)
+
 def find_galilei(sorted_inventors)	 
+  sorted = last_name_sort(inventors)
+  sorted.index do |inventor|
+    inventor.first == 'Galileo' && inventor.last == 'Galilei'
+  end
 	# using the sorted array from `last_name_sort`, find the index of `Galileo Galilei`
 end
--- ruby/arrays/inventor.rb
+++ ruby/arrays/inventor.rb
@@ -1,6 +1,6 @@
 class Inventor
   attr_reader :first, :last, :born, :passed
-
+  attr_writer :first
   def initialize(first, last, born, passed)
     @first = first
     @last = last
